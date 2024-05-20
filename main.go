2c4f21a1973f85d4079dd0be3fceb56f09e69f93
mcurvay <96953565+mcurvay@users.noreply.github.com>
2024-05-20 20:57:11 +0300

    made some improvements app is faster

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -21,20 +22,27 @@ func main() {
 	startPort := 1
 	endPort := 1024
 
+	var wg sync.WaitGroup
+
 	// Iterate over the port range
 	for port := startPort; port <= endPort; port++ {
-		address := ip + ":" + strconv.Itoa(port)
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			address := ip + ":" + strconv.Itoa(port)
 
-		// Try to connect to the port
-		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
+			// Try to connect to the port
+			conn, err := net.DialTimeout("tcp", address, 10*time.Second)
 
-		// If there's no error, the port is open
-		if err == nil {
-			fmt.Printf("Port %d is open\n", port)
-			err := conn.Close()
-			if err != nil {
-				return
+			// If there's no error, the port is open
+			if err == nil {
+				fmt.Printf("Port %d is open\n", port)
+				err := conn.Close()
+				if err != nil {
+					return
+				}
 			}
-		}
+		}(port)
 	}
+	wg.Wait()
 }
